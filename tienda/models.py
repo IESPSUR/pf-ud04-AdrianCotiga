e2d72af9f322b7d6546cc9eb2935c48a9569c16e
AdrianCotiga <adrian.cotiga.al@iespoligonosur.org>
2022-11-08 09:48:26 +0100

    fecha compra con hora

--- tienda/models.py
+++ tienda/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 
 class Marca(models.Model):
-    nombre = models.CharField(max_length=80)
+    nombre = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
 
 class Producto(models.Model):
-    nombre = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     unidades = models.IntegerField()
     precio = models.IntegerField()
     detalles = models.TextField(blank=True)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
 
 class Compra(models.Model):
-    fecha = models.DateField()
+    fecha = models.DateTimeField()
     unidades = models.ForeignKey(Producto, on_delete=models.CASCADE)
     importe = models.IntegerField()
 
     def __str__(self):
-        return self.fecha
+        return str(self.fecha)
