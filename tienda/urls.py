290e1ac247280accf4f06121f4c8f2f1817afe2c
AdrianCotiga <adrian.cotiga.al@iespoligonosur.org>
2022-11-14 10:17:41 +0100

    listado se muestra correctamente

--- tienda/urls.py
+++ tienda/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('tienda/', views.welcome, name='welcome'),
     path('tienda/admin/listado/', views.listado, name='listado'),
+    path('/tienda/admin/detalles/<int:pk>/', views.detalles, name='detalles'),
+    path('tienda/admin/edicion/', views.edicion, name='edcion'),
+    path('tienda/admin/eliminar/', views.eliminar, name='eliminar'),
+    path('tienda/admin/nuevo/<int:pk>/', views.nuevo, name='nuevo'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
-from tienda.models import Marca
+from tienda.models import Producto
 
 # Create your views here.
 def welcome(request):
     return render(request,'tienda/index.html', {})
 
 def listado(request):
-    marca = Marca.objects.all()
-    return render(request, 'tienda/admin/listado.html', { 'marca' : marca })
+    producto = Producto.objects.all()
+    return render(request, 'tienda/admin/listado.html', { 'productos' : producto })
 
+def detalles(request):
+    return render(request, 'tienda/admin/detalles.html', {})
+
+def edicion(request):
+    return render(request, 'tienda/admin/edicion.html', {})
+
+def eliminar(request):
+    return render(request, 'tienda/admin/eliminar.html', {})
+
+def nuevo (request):
+    return render(request, 'tienda/admin/nuevo.html', {})
